aa1c485fd0235ff99271da51d26cde9c8e04d30f
Allison _ <allimacq@gmail.com>
2021-03-17 16:29:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,60 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  #get '/' do
+  #end
+  
+   get '/articles' do
+    @articles = Article.all
+    
+    erb :index
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @id = params[:id]
+    @article = Article.find_by(id: @id.to_i)
+    
+    @title = @article.title
+    @content = @article.content
+  
+     erb :show
+  end
+  
+   post '/articles' do
+    @title = params[:title]
+    @content = params[:content]
+    
+    @new = Article.create(title: @title, content: @content)
+   
+    @id = @new.id
+    redirect to "/articles/#{@id}"
+  end
+  
+  get '/articles/:id/edit' do
+    @id = params[:id]
+    @article2 = Article.find_by(id: @id.to_i)
+    
+    @title = @article2.title
+    @content = @article2.content
+    
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    @id = params[:id]
+    @to_update = Article.find_by(id: @id.to_i)
+    
+    @to_update.title = params[:title]
+    save
+    @to_update.content = params[:content]
+    save
+    
+    #@to_update.update!(title: @title, content: @content)
+    
+  end
+  
 end
